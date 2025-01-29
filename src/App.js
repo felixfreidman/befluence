f6e21a311121956844affbd28374564a9cccbeb3
Felix Freidman <felixfreidman@gmail.com>
2025-01-29 14:51:42 +0300

    set up the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import classes from './index.module.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.App}>
+      {/* <Layout>
+        <Routes>
+          <Route path="/" element={<GreetingPage />} />
+          <Route path="/about_me" element={<AboutPage />} />
+        </Routes>
+      </Layout> */}
     </div>
   );
 }
